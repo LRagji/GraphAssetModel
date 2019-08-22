efe3cac29138f364436367b5206bfb9e39a3f26d
LaukikOnMac <22937426+LRagji@users.noreply.github.com>
2019-08-22 20:43:55 +0530

    Completed BitStream memory module

--- BitStream.js
+++ BitStream.js
@@ -1,25 +1,55 @@
+const bitsInByte = 8;
 
 module.exports = class BitStream {
 
     constructor(numberofBits) {
         this.getBit = this.getBit.bind(this);
         this.setBit = this.setBit.bind(this);
+        this.clearBit = this.clearBit.bind(this);
 
         let numberOfBytes = 0;
         if (numberofBits > 0) {
-            numberOfBytes = Math.ceil(Math.floor(numberofBits) / 8);
+            numberOfBytes = Math.ceil(Math.floor(numberofBits) / bitsInByte);
         }
         this._stream = new Uint8ClampedArray(numberOfBytes);
 
         this.memorySize = Uint8ClampedArray.BYTES_PER_ELEMENT * this._stream.length;
+        this._numberOfBits = numberofBits;
     }
 
     getBit(bitNumber) {
-
+        if (bitNumber < 0 || bitNumber >= this._numberOfBits) {
+            throw new Error('Invalid bit number ' + bitNumber);
+        }
+        let byteTrayIndex = Math.floor(bitNumber / bitsInByte);
+        let bitNo = ((bitNumber / bitsInByte) - byteTrayIndex) * bitsInByte;
+        let byteTray = this._stream[byteTrayIndex];
+        let bitmask = 1 << bitNo;
+        return (byteTray & bitmask) !== 0;
     }
 
     setBit(bitNumber) {
+        if (bitNumber < 0 || bitNumber >= this._numberOfBits) {
+            throw new Error('Invalid bit number ' + bitNumber);
+        }
+        let byteTrayIndex = Math.floor(bitNumber / bitsInByte);
+        let bitNo = ((bitNumber / bitsInByte) - byteTrayIndex) * bitsInByte;
+        let byteTray = this._stream[byteTrayIndex];
+        let bitmask = 1 << bitNo;
+        this._stream[byteTrayIndex] = byteTray |= bitmask;
+        return true;
+    }
 
+    clearBit(bitNumber) {
+        if (bitNumber < 0 || bitNumber >= this._numberOfBits) {
+            throw new Error('Invalid bit number ' + bitNumber);
+        }
+        let byteTrayIndex = Math.floor(bitNumber / bitsInByte);
+        let bitNo = ((bitNumber / bitsInByte) - byteTrayIndex) * bitsInByte;
+        let byteTray = this._stream[byteTrayIndex];
+        let bitmask = 1 << bitNo;
+        this._stream[byteTrayIndex] = byteTray &= ~bitmask;
+        return true;
     }
 
 }
--- BitStreamSpec.js
+++ BitStreamSpec.js
@@ -42,3 +42,82 @@ it('Bit Stream: Calculate correct memory size for negative bits', function (done
     expect(target.memorySize).to.equal(0);
     done();
 });
+
+it('Bit Stream: Get value of negative bit', function (done) {
+
+    let target = new targetType(10);
+    expect(() => target.getBit(-1)).to.throw('Invalid bit number -1');
+    done();
+});
+
+it('Bit Stream: Get value of bit outside the assigned bits', function (done) {
+
+    let target = new targetType(10);
+    expect(() => target.getBit(11)).to.throw('Invalid bit number 11');
+    done();
+});
+
+it('Bit Stream: Get value of bit zero with zero bits requested', function (done) {
+
+    let target = new targetType(0);
+    expect(() => target.getBit(0)).to.throw('Invalid bit number 0');
+    done();
+});
+
+it('Bit Stream: Set value of negative bit', function (done) {
+    let target = new targetType(10);
+    expect(() => target.setBit(-1)).to.throw('Invalid bit number -1');
+    done();
+});
+
+it('Bit Stream: Set value of bit outside the assigned bits', function (done) {
+    let target = new targetType(10);
+    expect(() => target.setBit(11)).to.throw('Invalid bit number 11');
+    done();
+});
+
+it('Bit Stream: Set value of bit zero with zero bits requested', function (done) {
+    let target = new targetType(0);
+    expect(() => target.setBit(0)).to.throw('Invalid bit number 0');
+    done();
+});
+
+it('Bit Stream: Clear value of negative bit', function (done) {
+    let target = new targetType(10);
+    expect(() => target.clearBit(-1)).to.throw('Invalid bit number -1');
+    done();
+});
+
+it('Bit Stream: Clear value of bit outside the assigned bits', function (done) {
+    let target = new targetType(10);
+    expect(() => target.clearBit(11)).to.throw('Invalid bit number 11');
+    done();
+});
+
+it('Bit Stream: Get Set Clear Bit Fields', function (done) {
+    const numberOfBits = 1000 * 1000 * 5;
+    let target = new targetType(numberOfBits);
+    let expected = "";
+    for (let ctr = 0; ctr < numberOfBits; ctr++) {
+        if (ctr % 2 == 0) {
+            target.setBit(ctr);
+            expected += '1';
+        }
+        else {
+            target.clearBit(ctr);
+            expected += '0';
+        }
+    }
+    let actual = "";
+    for (let ctr = 0; ctr < numberOfBits; ctr++) {
+        actual += target.getBit(ctr) ? '1' : '0';
+    }
+    expect(expected).to.be.equal(actual);
+    done();
+});
+
+it('Bit Stream: Clear value of bit zero with zero bits requested', function (done) {
+    let target = new targetType(0);
+    expect(() => target.clearBit(0)).to.throw('Invalid bit number 0');
+    done();
+});
