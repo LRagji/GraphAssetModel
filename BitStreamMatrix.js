8f89e2b59f93d89fb84a5135e5c538472ac07780
LaukikOnMac <22937426+LRagji@users.noreply.github.com>
2019-08-23 15:26:45 +0530

    Made compatible with AssetModel Interface.

--- BitStreamMatrix.js
+++ BitStreamMatrix.js
@@ -6,6 +6,10 @@ module.exports = class BitStream {
         this.getBit = this.getBit.bind(this);
         this.setBit = this.setBit.bind(this);
         this.clearBit = this.clearBit.bind(this);
+        this.read = this.read.bind(this);
+        this.mark = this.mark.bind(this);
+        this._generateLoop = this._generateLoop.bind(this);
+        this._convertToSerialIndex = this._convertToSerialIndex.bind(this);
 
         this.dimensionSize = {
             "YMax": YMax,
@@ -13,8 +17,8 @@ module.exports = class BitStream {
             "ZMax": ZMax
         };
 
-        this._numberOfBits= YMax * XMax * ZMax;
-        
+        this._numberOfBits = YMax * XMax * ZMax;
+
         let numberOfBytes = 0;
         if (this._numberOfBits > 0) {
             numberOfBytes = Math.ceil(Math.floor(this._numberOfBits) / bitsInByte);
@@ -59,4 +63,38 @@ module.exports = class BitStream {
         return true;
     }
 
+    read(y, x, z, iteratorCallback) {
+        //TODO: Check for Negative number for x,y,z axes and should be less than Xmax, Ymax and Zmax
+        this._generateLoop(y, this.dimensionSize.YMax, (YIdx) => {
+            return this._generateLoop(x, this.dimensionSize.XMax, (XIdx) => {
+                return this._generateLoop(z, this.dimensionSize.ZMax, (ZIdx) => {
+                    let serialIdx = this._convertToSerialIndex(YIdx, XIdx, ZIdx,this.dimensionSize.XMax,this.dimensionSize.YMax);
+                    if (this.getBit(serialIdx)) {
+                        return iteratorCallback(YIdx, XIdx, ZIdx);
+                    }
+                });
+            });
+        });
+    }
+
+    _generateLoop(parameter, defaultEP, iteratorFunction) {
+        //Scan Or Fetch Mode
+        for (let counter = (parameter == undefined ? 0 : parameter); counter < (parameter == undefined ? defaultEP : (parameter + 1)); counter++) {
+            if (iteratorFunction(counter) === false) return false;
+        }
+    }
+
+    mark(y, x, z) {
+        if (y == undefined || x == undefined || z == undefined) {
+            throw new Error("One or more parameters are null/undefined.");
+        }
+        let serialIdx = this._convertToSerialIndex(y, x, z,this.dimensionSize.XMax,this.dimensionSize.YMax);
+        this.setBit(serialIdx);
+    }
+
+    _convertToSerialIndex(y, x, z, xLength, yLength) {
+        let twoDimensionSerialIndex = (y * xLength) + x;
+        let threeDimensionSerialIndex = twoDimensionSerialIndex + (z * xLength * yLength);
+        return threeDimensionSerialIndex;
+    }
 }
--- BitStreamMatrixSpec.js
+++ BitStreamMatrixSpec.js
@@ -43,51 +43,6 @@ it('Bit Stream: Calculate correct memory size for negative bits', function (done
     done();
 });
 
-it('Bit Stream: Get value of negative bit', function (done) {
-
-    let target = new targetType(10,1,1);
-    expect(() => target.getBit(-1)).to.throw('Invalid bit number -1');
-    done();
-});
-
-it('Bit Stream: Get value of bit outside the assigned bits', function (done) {
-
-    let target = new targetType(10,1,1);
-    expect(() => target.getBit(11)).to.throw('Invalid bit number 11');
-    done();
-});
-
-it('Bit Stream: Get value of bit zero with zero bits requested', function (done) {
-
-    let target = new targetType(0,1,1);
-    expect(() => target.getBit(0)).to.throw('Invalid bit number 0');
-    done();
-});
-
-it('Bit Stream: Set value of negative bit', function (done) {
-    let target = new targetType(10,1,1);
-    expect(() => target.setBit(-1)).to.throw('Invalid bit number -1');
-    done();
-});
-
-it('Bit Stream: Set value of bit outside the assigned bits', function (done) {
-    let target = new targetType(10,1,1);
-    expect(() => target.setBit(11)).to.throw('Invalid bit number 11');
-    done();
-});
-
-it('Bit Stream: Set value of bit zero with zero bits requested', function (done) {
-    let target = new targetType(0,1,1);
-    expect(() => target.setBit(0)).to.throw('Invalid bit number 0');
-    done();
-});
-
-it('Bit Stream: Clear value of negative bit', function (done) {
-    let target = new targetType(10,1,1);
-    expect(() => target.clearBit(-1)).to.throw('Invalid bit number -1');
-    done();
-});
-
 it('Bit Stream: Clear value of bit outside the assigned bits', function (done) {
     let target = new targetType(10,1,1);
     expect(() => target.clearBit(11)).to.throw('Invalid bit number 11');
@@ -119,5 +74,242 @@ it('Bit Stream: Get Set Clear Bit Fields', function (done) {
 it('Bit Stream: Clear value of bit zero with zero bits requested', function (done) {
     let target = new targetType(0,1,1);
     expect(() => target.clearBit(0)).to.throw('Invalid bit number 0');
+    done();
+});
+
+//================================================================================================
+
+function convertToString(y, x, z) {
+    return y + ',' + x + ',' + z;
+}
+
+it('Bit Stream Matrix: Validate markings are correct', function (done) {
+    let size = 10;
+    let target = new targetType(size, size, size);
+
+    target.mark(0, 0, 0);
+    target.mark(9, 9, 9);
+
+    let hits = [];
+    target.read(undefined, undefined, undefined, (y, x, z) => {
+        hits.push(convertToString(y, x, z));
+        return true;
+    });
+
+    expect(hits).to.deep.equal(['0,0,0', '9,9,9']);
+    done();
+});
+
+it('Bit Stream Matrix: Validate iterator stops when asked to', function (done) {
+    let size = 10;
+    let target = new targetType(size, size, size);
+
+    target.mark(0, 0, 0);
+    target.mark(9, 9, 9);
+
+    let hits = [];
+    target.read(undefined, undefined, undefined, (y, x, z) => {
+        hits.push(convertToString(y, x, z));
+        return false;
+    });
+
+    expect(hits).to.deep.equal(['0,0,0']);
+    done();
+});
+
+it('Bit Stream Matrix: Mark and Get same index on 3rd Axis', function (done) {
+    let size = 10;
+    let x = 0, y = 0, defaultChar = '0', markchar = 'D';
+    let target = new targetType(size, size, size, defaultChar, markchar);
+    let expectedResult = [];
+    for (let counter = 0; counter < target.dimensionSize.ZMax; counter++) {
+        target.mark(y, x, counter);
+        expectedResult.push(convertToString(y, x, counter));
+    }
+
+    let actualResult = [];
+    for (let counter = 0; counter < target.dimensionSize.ZMax; counter++) {
+        target.read(y, x, counter, (y, x, z) => {
+            actualResult.push(convertToString(y, x, counter));
+            return true
+        });
+    }
+    expect(actualResult).to.deep.equal(expectedResult);
+    done();
+});
+
+it('Bit Stream Matrix: Mark and Get same index on 2nd Axis', function (done) {
+    let size = 10;
+    let z = 0, y = 0, defaultChar = '0', markchar = 'D';;
+    let target = new targetType(size, size, size, defaultChar, markchar);
+    let expectedResult = [];
+    for (let counter = 0; counter < target.dimensionSize.XMax; counter++) {
+        target.mark(y, counter, z);
+        expectedResult.push(convertToString(y, counter, z));
+    }
+
+    let actualResult = [];
+    for (let counter = 0; counter < target.dimensionSize.XMax; counter++) {
+        target.read(y, counter, z, (y, x, z) => {
+            actualResult.push(convertToString(y, counter, z));
+            return true
+        });
+    }
+    expect(actualResult).to.deep.equal(expectedResult);
+    done();
+});
+
+it('Bit Stream Matrix: Mark and Get same index on 1st Axis', function (done) {
+    let size = 10;
+    let z = 0, x = 0, defaultChar = '0', markchar = 'D';;
+    let target = new targetType(size, size, size, defaultChar, markchar);
+    let expectedResult = [];
+    for (let counter = 0; counter < target.dimensionSize.YMax; counter++) {
+        target.mark(counter, x, z);
+        expectedResult.push(convertToString(counter, x, z));
+    }
+
+    let actualResult = [];
+    for (let counter = 0; counter < target.dimensionSize.YMax; counter++) {
+        target.read(counter, x, z, (y, x, z) => {
+            actualResult.push(convertToString(counter, x, z));
+            return true
+        });
+    }
+    expect(actualResult).to.deep.equal(expectedResult);
+    done();
+});
+
+it('Bit Stream Matrix: Mark and Get 1Dimension Array on 1st Axis', function (done) {
+    let size = 10;
+    let z = 0, x = 0, defaultChar = '0', markchar = 'D';
+    let target = new targetType(size, size, size, defaultChar, markchar);
+    let expectedArray = [];
+    for (let counter = 0; counter < target.dimensionSize.YMax; counter++) {
+        target.mark(counter, x, z, markchar);
+        expectedArray.push(counter);
+    }
+
+    let actualResult = [];
+    target.read(undefined, x, z, (y, x, z) => {
+        actualResult.push(y);
+        return true
+    })
+
+    expect(actualResult).to.deep.equal(expectedArray);
+    done();
+});
+
+it('Bit Stream Matrix: Mark and Get 1Dimension Array on 2nd Axis', function (done) {
+    let size = 10;
+    let z = 0, y = 0, defaultChar = '0', markchar = 'D';
+    let target = new targetType(size, size, size, defaultChar, markchar);
+    let expectedArray = [];
+    for (let counter = 0; counter < target.dimensionSize.XMax; counter++) {
+        target.mark(y, counter, z, markchar);
+        expectedArray.push(counter);
+    }
+
+    let actualResult = [];
+    target.read(y, undefined, z, (y, x, z) => {
+        actualResult.push(x);
+        return true
+    })
+
+    expect(actualResult).to.deep.equal(expectedArray);
+    done();
+});
+
+it('Bit Stream Matrix: Mark and Get 1Dimension Array on 3rd Axis', function (done) {
+    let size = 10;
+    let x = 0, y = 0, defaultChar = '0', markchar = 'D';
+    let target = new targetType(size, size, size, defaultChar, markchar);
+    let expectedArray = [];
+    for (let counter = 0; counter < target.dimensionSize.ZMax; counter++) {
+        target.mark(y, x, counter);
+        expectedArray.push(counter);
+    }
+
+    let actualResult = [];
+    target.read(y, x, undefined, (y, x, z) => {
+        actualResult.push(z);
+        return true
+    })
+
+    expect(actualResult).to.deep.equal(expectedArray);
+    done();
+});
+
+it('Bit Stream Matrix: Mark and Get 2Dimension on 2nd & 3rd Axis', function (done) {
+    let size = 10;
+    let y = 0, defaultChar = '0', markchar = 'D';
+    let target = new targetType(size, size, size, defaultChar, markchar);
+    let expectedArray = [];
+    for (let Xcounter = 0; Xcounter < target.dimensionSize.XMax; Xcounter++) {
+        for (let counter = 0; counter < target.dimensionSize.ZMax; counter++) {
+            target.mark(y, Xcounter, counter);
+            expectedArray.push(convertToString(y, Xcounter, counter));
+        }
+    }
+
+    let actualResult = [];
+    target.read(y, undefined, undefined, (y, x, z) => {
+        actualResult.push(convertToString(y, x, z));
+        return true
+    })
+
+    expect(actualResult).to.deep.equal(expectedArray);
+    done();
+});
+
+it('Bit Stream Matrix: Mark and Get 2Dimension on 1st & 2nd Axis', function (done) {
+    let size = 10;
+    let z = 0, defaultChar = '0', markchar = 'D';
+    let target = new targetType(size, size, size, defaultChar, markchar);
+    let expectedArray = [];
+    for (let Ycounter = 0; Ycounter < target.dimensionSize.YMax; Ycounter++) {
+        for (let counter = 0; counter < target.dimensionSize.XMax; counter++) {
+            target.mark(Ycounter, counter, z);
+            expectedArray.push(convertToString(Ycounter, counter, z));
+        }
+    }
+
+    let actualResult = [];
+    target.read(undefined, undefined, z, (y, x, z) => {
+        actualResult.push(convertToString(y, x, z));
+        return true
+    })
+
+    expect(actualResult).to.deep.equal(expectedArray);
+    done();
+});
+
+it('Bit Stream Matrix: Mark and Get 2Dimension on 1st & 3rd Axis', function (done) {
+    let size = 10;
+    let x = 0, defaultChar = '0', markchar = 'D';
+    let target = new targetType(size, size, size, defaultChar, markchar);
+    let expectedArray = [];
+    for (let Ycounter = 0; Ycounter < target.dimensionSize.YMax; Ycounter++) {
+        for (let counter = 0; counter < target.dimensionSize.ZMax; counter++) {
+            target.mark(Ycounter, x, counter);
+            expectedArray.push(convertToString(Ycounter, x, counter));
+        }
+    }
+
+    let actualResult = [];
+    target.read(undefined, x, undefined, (y, x, z) => {
+        actualResult.push(convertToString(y, x, z));
+        return true
+    })
+
+    expect(actualResult).to.deep.equal(expectedArray);
+    done();
+});
+
+it('Bit Stream Matrix: Validate negative marking', function (done) {
+    let size = 10;
+    let target = new targetType(size, size, size);
+
+    expect(() =>  target.mark(-1, 0, 0)).to.throw('Invalid bit number -10');
     done();
 });
